e952f2ff5508fc6f4fb374c54ea334f24d0c02b5
DevEdu <quisperojasedu@gmail.com>
2026-04-12 09:25:18 -0500

    feat: se implementa listado de buses desde SQL Server usando stored procedure

---
+++ BusTicketSystem/Controllers/BusesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BusTicketSystem.Repositories;
+
+namespace BusTicketSystem.Controllers
+{
+    public class BusesController : Controller
+    {
+        private readonly BusRepository _busRepository;
+
+        public BusesController(BusRepository busRepository)
+        {
+            _busRepository = busRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var buses = _busRepository.ListarBuses();
+            return View(buses);
+        }
+    }
+}
